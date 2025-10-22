5d097307227f249e57cb358e8dae5fb3b9b06421
DavidSaez21 <david04mingla@gmail.com>
2025-10-22 19:31:31 +0200

    cambios titulo

---
+++ ProjectRab_Project/Assets/RAB_Root/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleVolume : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = audioSource.volume;
+    }
+
+    public void ChangeVolume(float value)
+    {
+        audioSource.volume = value;
+    }
+}   

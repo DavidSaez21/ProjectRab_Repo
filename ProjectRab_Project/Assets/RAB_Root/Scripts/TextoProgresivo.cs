1dc3ab4944b605a2225569f0e8f542bc82128b04
RubiHD <rubi.de.rankawa@gmail.com>
2025-10-22 21:08:25 +0200

    Ajustes Menu Victoria

--- ProjectRab_Project/Assets/RAB_Root/Scripts/TextoProgresivo.cs
+++ ProjectRab_Project/Assets/RAB_Root/Scripts/TextoProgresivo.cs
@@ -28,7 +28,7 @@ public class TextoProgresivo : MonoBehaviour
             }
 
             // Espera hasta que el jugador presione cualquier tecla
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(10f);
         }
     }
 }
